75537d0d0337b5017494ec99d775767cdfb65be5
chiragsrvstv <chirag.srivastava@live.com>
2019-09-19 22:14:04 +0530

    API connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express       = require('express'),
       app           = express(),
-      bodyParser    = require('body-parser');
+      bodyParser    = require('body-parser'),
+      request       = require('request');
 
 // setting up server to respond requests at localhost:3000
 app.listen(3000, function() {
@@ -9,9 +10,44 @@ app.listen(3000, function() {
 
 //routes
 
+// landing Page
+app.get("/", function (req, res) {
+  res.redirect("/tonight");
+
+})
+
 //index route
-
 app.get("/tonight", function (req, res) {
-  res.render("index.ejs")
-
+  res.render("index.ejs");
 });
+
+//movies route
+app.get("/tonight/movies", function (req, res) {
+  res.render("movies.ejs");
+});
+
+//tvseries route
+app.get("/tonight/tvseries", function (req, res) {
+  res.render("tvseries.ejs");
+});
+
+// api connection
+app.get("/tonight/results", function (req, res) {
+
+
+  url = 'https://api.themoviedb.org/3/search/movie?api_key=90dc517176585a03c348c93afdd70126&query=John+Wick'
+  request(url, function (error, response, body) {
+    if(!error && response.statusCode == 200){
+      res.send(JSON.parse(body));
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
