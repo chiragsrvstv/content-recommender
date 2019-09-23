5de5ef3704be5c24281c0c3873316d98a832f96b
chiragsrvstv <chirag.srivastava@live.com>
2019-09-23 13:27:17 +0530

    adding partials

--- app.js
+++ app.js
@@ -21,28 +21,42 @@ app.get("/tonight", function (req, res) {
   res.render("index.ejs");
 });
 
-//movies route
+// //movies route
+// app.get("/tonight/movies", function (req, res) {
+//   res.render("movies.ejs");
+// });
+
+// //tvseries route
+// app.get("/tonight/tvseries", function (req, res) {
+//   res.render("tvseries.ejs");
+// });
+
+// api connection for movies
 app.get("/tonight/movies", function (req, res) {
-  res.render("movies.ejs");
-});
-
-//tvseries route
-app.get("/tonight/tvseries", function (req, res) {
-  res.render("tvseries.ejs");
-});
-
-// api connection
-app.get("/tonight/results", function (req, res) {
-
-
-  url = 'https://api.themoviedb.org/3/search/movie?api_key=90dc517176585a03c348c93afdd70126&query=John+Wick'
-  request(url, function (error, response, body) {
+  //const random = Math.floor(Math.random()*1000)
+  //api key
+  key = '90dc517176585a03c348c93afdd70126'
+  const url1 = 'https://api.themoviedb.org/3/discover/movie?api_key=90dc517176585a03c348c93afdd70126&language=en-US&sort_by=popularity.desc&region=IN&year=2019&include_adult=true&include_video=false&page=1'
+  request(url1, function (error, response, body) {
     if(!error && response.statusCode == 200){
-      res.send(JSON.parse(body));
+      const data = JSON.parse(body);
+      console.log(data);
+      res.render("movies.ejs", {data: data});
     }
   });
 });
 
+// api connection for tv
+app.get("/tonight/tvseries", function (req, res) {
+  const url2 = 'https://api.themoviedb.org/3/discover/tv?api_key=90dc517176585a03c348c93afdd70126&language=en-US&region=IN&sort_by=popularity.desc&page=1&include_null_first_air_dates=false'
+  request(url2, function (error, response, body) {
+    if(!error && response.statusCode == 200){
+      const data = JSON.parse(body);
+      //console.log(data);
+      res.render("tvseries.ejs", {data: data});
+    }
+  });
+});
 
 
 
