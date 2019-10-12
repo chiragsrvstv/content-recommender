af39bd238fe2d83a7a9ca26b7519b9559dd2d203
chiragsrvstv <chirag.srivastava@live.com>
2019-10-12 23:24:12 +0530

    tv series filters

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.listen(3000, function() {
 });
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-//routes
+
+//-----------------------routes--------------------------------
 
 // landing Page
 app.get("/", function(req, res) {
@@ -21,33 +22,7 @@ app.get("/tonight", function(req, res) {
   res.render("index.ejs");
 });
 
-//movies result route
-app.post("/tonight/movies/moviesresults", function(req, res) {
-  var genres = req.body.genre;
-  console.log(genres);
-  // var gen = []
-  // console.log(genres);
-  // for (var key in genres){
-  //   gen.push(genres[key]);
-  // };
-  // console.log(gen);
-  const url3 =
-    "https://api.themoviedb.org/3/discover/movie?api_key=90dc517176585a03c348c93afdd70126&language=en-US&sort_by=popularity.desc&region=IN,US&include_adult=true&include_video=false&page=1&&with_genres="+genres;
-  request(url3, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const data = JSON.parse(body);
-      res.render("movieResults.ejs", { data: data });
-      //console.log(data);
-    }
-  });
-});
-
-// //tvseries route
-// app.get("/tonight/tvseries", function (req, res) {
-//   res.render("tvseries.ejs");
-// });
-
-// api connection for movies
+// movies route and api connection
 app.get("/tonight/movies", function(req, res) {
   //const random = Math.floor(Math.random()*1000)
   //api key
@@ -63,7 +38,23 @@ app.get("/tonight/movies", function(req, res) {
   });
 });
 
-// api connection for tv
+//movies result route
+app.post("/tonight/movies/moviesresults", function(req, res) {
+  var genres = req.body.genre;
+  console.log(genres);
+  const url3 =
+    "https://api.themoviedb.org/3/discover/movie?api_key=90dc517176585a03c348c93afdd70126&language=en-US&sort_by=popularity.desc&region=IN,US&include_adult=true&include_video=false&page=1&with_genres=" +
+    genres;
+  request(url3, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.render("movieResults.ejs", { data: data });
+      //console.log(data);
+    }
+  });
+});
+
+// tv series route and api connection
 app.get("/tonight/tvseries", function(req, res) {
   const url2 =
     "https://api.themoviedb.org/3/discover/tv?api_key=90dc517176585a03c348c93afdd70126&language=en-US&region=IN&sort_by=popularity.desc&page=1&include_null_first_air_dates=false";
@@ -75,3 +66,18 @@ app.get("/tonight/tvseries", function(req, res) {
     }
   });
 });
+
+// tvseries result oute
+app.post("/tonight/tvserie/tvresults", function(req, res) {
+  const genres = req.body.genre;
+  const tvResultsUrl = "https://api.themoviedb.org/3/discover/tv?api_key=90dc517176585a03c348c93afdd70126&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_genres="+
+  genres;
+  request(tvResultsUrl, function (error, response, body) {
+    if(!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.render("tvresults.ejs", {data: data});
+    }
+  });
+});
+
+
